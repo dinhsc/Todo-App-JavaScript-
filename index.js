54d19c1c9e104c0a68ace33f370962d7458fa6a0
dinhsc <dinhc692@gmail.com>
2024-09-24 18:58:30 +0200

    updating index from adding new tasks

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ function adding_task() { // adding_task(): void
         
         // Ajoute un gestionnaire de clic sur l'ensemble du conteneur pour trigger la checkbox
         task_container.addEventListener("click", () => {
-            console.log(new_task.checked);
             new_task.checked = !new_task.checked; // Toggle de la checkbox
-            console.log(new_task.checked);
+            
             // Met à jour le statut de la tâche dans la liste
             for (let i = 0; i < tasks_list.length; i++) {
                 if (tasks_list[i].id == new_task.id) {
@@ -53,8 +52,6 @@ function adding_task() { // adding_task(): void
         output_task.append(task_container);
 
         input_task.value = "";
-        console.log("INDEXd4", GLOBAL_INDEX);
-
         GLOBAL_INDEX++;
 
     } else {
@@ -68,19 +65,16 @@ function adding_task() { // adding_task(): void
 // Traiter le cas des suppression : Mettre à jours les index
 function delete_task() { // delete_task(): void
     output_task.textContent = "";
-    const complete_task = tasks_list.filter(tasks_list => tasks_list.completed = true);
-
-    // On retourne une nouvelle liste de tâches ne contenant pas les tâches accompli
     const undone_task = tasks_list.filter(tasks_list => tasks_list.completed != true); // return : liste de tâches
-    console.log("TACHE OK", complete_task)
     console.log("Liste actuelle :", tasks_list);
     for (let index = 0; index < undone_task.length; index++) {
+        // On crée un nouveau tableau contenant uniquement les tâches non effectuées
         const update_task_container = document.createElement("div");
         const update_task = document.createElement("input");
         update_task.type = "checkbox";
-        update_task.id = index; //undone_task[index].id;
-
+        
         // On met à jour les bons index du tableau
+        update_task.id = index; 
         undone_task[index].id = index;
 
         const update_task_label = document.createElement("label");
@@ -88,30 +82,29 @@ function delete_task() { // delete_task(): void
 
         // Réattribuer le gestionnaire d'événements pour la nouvelle tâche (on rentre ici après avoir supprimé au moins 1 fois)
         update_task_container.addEventListener("click", () => {
-            // (on rentre ici après avoir supprimé au moins 1 fois)
+            // Point important : La checkbox est coché visuellement mais cet événement est nécessaire
+            // pour que le back soit à jour égalements
+            // On rentre ici après avoir supprimé au moins une fois
             update_task.checked = !update_task.checked;
             for (let i = 0; i < tasks_list.length; i++) {
                 if (tasks_list[i].id == update_task.id) {
                     tasks_list[i].completed = update_task.checked;
                 }
             }
-            checkDeleteButton();
-
-            undone_task.push({
-                id: update_task.id,
-                name: update_task_label.textContent,
-                completed: false
-            })
-            // Point important : La checkbox est coché visuellement mais cet événement est nécessaire
-            // pour que le back soit à jour égalements
-            GLOBAL_INDEX = 7;
-            
+            checkDeleteButton();  
         });
-        console.log("INDEX4444", GLOBAL_INDEX);
+
         update_task_container.append(update_task);
         update_task_container.append(update_task_label);
         output_task.append(update_task_container);
+
+        // On prend le dernier index de la boucle + 1 (ou la dernière longueur du tableau undone_task)
+        // Pour préserver la suite logique des prochaines tâches ajoutés
+        GLOBAL_INDEX = undone_task.length;
+
     }
+    
+    if (undone_task == 0) { GLOBAL_INDEX = 0}
     tasks_list = undone_task;
     // On désactive à nouveau le bouton après avoir supprimé les tâches accomplies
     checkDeleteButton();
@@ -121,6 +114,3 @@ function delete_task() { // delete_task(): void
 
 add_button.addEventListener("click", adding_task);
 delete_button.addEventListener("click", delete_task);
-
-
-// Prochaine étape : Mettre les index dans l'ordre lors de l'ajout (après la suppréssion)
